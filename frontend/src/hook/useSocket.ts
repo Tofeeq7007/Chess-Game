a91ccd229eb7c9d2af1a4b4d8fab345f561140cd
Tofee raja <tofeeqraja9100@gmail.com>
2025-11-02 15:46:02 +0530

    use Socket

--- frontend/src/hook/useSocket.ts
+++ frontend/src/hook/useSocket.ts
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react"
 const WS_URL = import.meta.env.VITE_BACKEND_URL as string || 'ws://localhost:8080';
 export const useSocket = ()=>{
     const [socket , setSocket] = useState<WebSocket | null>(null);
-
+    console.log("WS_URL", WS_URL);
     useEffect(()=>{
         const ws  = new WebSocket(WS_URL);
         ws.onopen=()=>{
